c335263133fb21f5ad8d34137e8191db39c8e039
osteensouthpaw <osteensouthpaw@gmail.com>
2024-03-04 14:02:31 +0300

    This is the first commit

---
+++ src/main/java/com/omega/hogwatsartifactsonline/HogwatsArtifactsOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.omega.hogwatsartifactsonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HogwatsArtifactsOnlineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HogwatsArtifactsOnlineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/omega/hogwatsartifactsonline/HogwatsArtifactsOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omega.hogwatsartifactsonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HogwatsArtifactsOnlineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
